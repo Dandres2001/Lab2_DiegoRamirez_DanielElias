8e05024a9dfe8869c27b15661f9481a39c0ece14
Dandres2001 <dandres21nw@gmail.com>
2021-02-25 18:42:11 -0600

    .

--- Lab2_DiegoRamirez_DanielElias/Controllers/DrugsController.cs
+++ Lab2_DiegoRamirez_DanielElias/Controllers/DrugsController.cs
@@ -57,7 +57,7 @@ namespace Lab2_DiegoRamirez_DanielElias.Controllers
                         newDrug.Name = fields[1];
                         newDrug.Description = fields[2];
                         newDrug.Factory = fields[3];
-                        newDrug.Price = Convert.ToDouble(fields[4]);
+                        newDrug.Price = fields[4];
                         newDrug.Stock = Convert.ToInt32(fields[5]);
                         Singleton.Instance.DrugsList.AddLast(newDrug);
                     }
