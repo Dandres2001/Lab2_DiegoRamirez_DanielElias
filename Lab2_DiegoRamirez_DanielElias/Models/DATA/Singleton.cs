b266438cece219227a7d9a7a00bf9e9f15b4aeac
DanielElias12 <daniel_alejan123@hotmail.com>
2021-02-25 18:46:53 -0600

    1

---
+++ Lab2_DiegoRamirez_DanielElias/Models/DATA/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_DiegoRamirez_DanielElias.Models.DATA
+{
+    public class Singleton
+    {
+    }
+}
