60f5733a45ca0cb6ae7646e82bc1eba279c84b3a
Dandres2001 <dandres21nw@gmail.com>
2021-02-27 14:16:57 -0600

    new Master

--- Lab2_DiegoRamirez_DanielElias/Models/DATA/Singleton.cs
+++ Lab2_DiegoRamirez_DanielElias/Models/DATA/Singleton.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibreriaRD2;
 
-namespace Lab2_DiegoRamirez_DanielElias.Models.DATA
+namespace Lab2_DiegoRamirez_DanielElias.Models.Data
 {
-    public class Singleton
+    public sealed class Singleton
     {
+        private readonly static Singleton _instance = new Singleton();
+        public Manual_List<Drug> DrugsList;
+       
+        private Singleton()
+        {
+            
+            DrugsList = new Manual_List<Drug>();
+          
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
     }
 }
