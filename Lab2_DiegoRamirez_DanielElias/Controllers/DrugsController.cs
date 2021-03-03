8f7b4483cf03c381cc057d1a9a0633800f993e3c
Dandres2001 <dandres21nw@gmail.com>
2021-03-02 19:47:12 -0600

    Order

--- Lab2_DiegoRamirez_DanielElias/Controllers/DrugsController.cs
+++ Lab2_DiegoRamirez_DanielElias/Controllers/DrugsController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
-
+using Lab2_DiegoRamirez_DanielElias.Models;
 using Lab2_DiegoRamirez_DanielElias.Models.Data;
 using Microsoft.VisualBasic;
 using System.IO;
@@ -18,14 +18,130 @@ namespace Lab2_DiegoRamirez_DanielElias.Controllers
 {
     public class DrugsController : Controller
     {
-
+        
         IWebHostEnvironment hostingEnvironment;
         public DrugsController(IWebHostEnvironment hostingEnvironment)
         {
             this.hostingEnvironment = hostingEnvironment;
 
         }
+         
+
         [HttpGet]
+        public ActionResult AddToOrder(int id)
+        {
+            Drug selected;
+            int i;
+            for ( i = 0; Singleton.Instance.DrugsList.Length > i; i++)
+            {
+                selected = Singleton.Instance.DrugsList.ElementAt(i);
+
+
+                if (selected.ID == id)
+                { 
+                    break;
+                }
+
+            }
+
+            selected = Singleton.Instance.DrugsList.ElementAt(i);
+            return View(selected);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddToOrder(int id, IFormCollection collection)
+        {
+
+
+            int i;
+            try
+            {
+                Drug selected;
+
+                for (i = 0; Singleton.Instance.DrugsList.Length > i; i++)
+                {
+                    selected = Singleton.Instance.DrugsList.ElementAt(i);
+                    if (selected.ID == id)
+                    { 
+                        break;
+                    }
+                }
+
+                int quantity = Convert.ToInt32(collection["OrderedQuantity"]);
+                selected = Singleton.Instance.DrugsList.ElementAt(i);
+                selected.OrderedQuantity = quantity;
+
+                if ((selected.OrderedQuantity <= selected.Stock)&& quantity != 0)
+                {
+                    selected.Stock = selected.Stock - quantity ;
+                    Singleton.Instance.OrderedDrugs.AddLast(selected);
+                }
+                else
+                {
+                    OutOfStockMessage();
+                }
+
+                return RedirectToAction(nameof(AddToOrder));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult OutOfStockMessage()
+        {
+            TempData["alertMessage"] = "OUT OF STOCK!";
+            return View();
+        }
+        public ActionResult ExportOrder()
+        {
+            TempData["alertMessage"] = "Order made successfully!";
+            return View();
+        }
+        [HttpGet]
+
+        [HttpGet]
+        public ActionResult FinalOrder()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FinalOrder(IFormCollection collection)
+        {
+            
+            Drug selected;
+            double totalPrice = 0; ;
+            string OrderText = "";
+            OrderText += "Name: " + collection["Name"] + "\n";
+            OrderText += "Address" + collection["Address"] + "\n";
+            OrderText += "NIT " + collection["NIT"] + "\n";
+            OrderText +=  "\n";
+            OrderText += "Listed order: " + "\n" + "\n";
+
+
+            for (int i = 0; Singleton.Instance.OrderedDrugs.Length > i; i++)
+            {
+                double price;
+
+                selected = Singleton.Instance.OrderedDrugs.ElementAt(i);
+                price = Convert.ToDouble(selected.Price.Substring(1, selected.Price.Length-1));
+                price = price * selected.OrderedQuantity;
+               
+                OrderText += selected.Name + " x " + selected.OrderedQuantity.ToString() + ": $" + price.ToString() + "\n";
+                totalPrice += price;
+            }
+           
+            OrderText += "\n" + "Total: $" + totalPrice.ToString();
+
+
+            return File(Encoding.UTF8.GetBytes(OrderText), "text/csv", "Order.txt");
+            return View();
+        }
+      
+
         public ActionResult ReadFile()
         {
 
